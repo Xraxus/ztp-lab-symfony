b9b30b3faecd1d7c0f56c9fcdb8e707e4662bdba
Kamil Kobylarz <xraxus@gmail.com>
2025-04-09 08:14:53 +0200

    fix phpunit.dist

---
+++ app/src/Controller/HelloController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Hello controller.
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+/**
+ * Class HelloController.
+ */
+#[Route('/hello')]
+class HelloController extends AbstractController
+{
+    /**
+     * Index action.
+     *
+     * @return Response HTTP response
+     */
+    #[Route(
+        '',
+        name: 'hello_index',
+        methods: 'GET'
+    )]
+    public function index(): Response
+    {
+        return new Response();
+    }
+}
